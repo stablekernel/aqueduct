e35b3ef90edb83070ae5285e894882d736f80f33
Joe Conway <joe.conway@stablekernel.com>
2015-10-26 22:58:56 -0400

    Combined auth with monadart, no reason ot keep them separate

--- lib/monadart.dart
+++ lib/monadart.dart
@@ -5,8 +5,10 @@ import 'dart:async';
 import 'dart:mirrors';
 import 'dart:isolate';
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:pbkdf2/pbkdf2.dart';
+import 'dart:math';
 import 'package:http_server/http_server.dart';
-
 export 'package:http_server/http_server.dart';
 
 part 'base/resource_pattern.dart';
@@ -18,3 +20,10 @@ part 'base/application.dart';
 part 'base/request_handler.dart';
 part 'base/controller_routing.dart';
 part 'base/http_response_exception.dart';
+
+part 'auth/authenticator.dart';
+part 'auth/protocols.dart';
+part 'auth/token_generator.dart';
+part 'auth/client.dart';
+part 'auth/auth_controller.dart';
+part 'auth/authorization_parser.dart';
