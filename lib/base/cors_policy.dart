6a244926e228b1dd430f9f75a2a538146c1efecf
Joe Conway <joe.conway@stablekernel.com>
2016-06-07 17:12:38 -0400

    Add x-forwarded-for

--- lib/base/cors_policy.dart
+++ lib/base/cors_policy.dart
@@ -6,7 +6,7 @@ class CORSPolicy {
   List<String> exposedResponseHeaders = [];
 
   List<String> allowedMethods = ["POST", "PUT", "DELETE", "GET"];
-  List<String> allowedRequestHeaders = ["authorization", "x-requested-with", "content-type", "accept"];
+  List<String> allowedRequestHeaders = ["authorization", "x-requested-with", "content-type", "accept", "x-forwarded-for"];
   int cacheInSeconds = 3600;
 
   Map<String, dynamic> headersForRequest(ResourceRequest request) {
