fb657ef4ff6b004ea4278ab1f5fc0e114d64cf3e
Joe Conway <joe.conway@stablekernel.com>
2016-07-02 10:18:01 -0400

    Addressing comment

--- lib/base/body_decoder.dart
+++ lib/base/body_decoder.dart
@@ -9,15 +9,15 @@ class HTTPBodyDecoder {
   /// The set of available decoders for HTTP request data.
   ///
   /// The HTTP header for Content-Type is broken into two pieces, primary type (e.g., 'application') and subtype (e.g., 'json').
-  /// The primary type is the first key in [decoders], and then the subtype is used to return the specific encoding
+  /// The primary type is the first key in [_decoders], and then the subtype is used to return the specific encoding
   /// function from this map. Decoders take a [HttpRequest] and return the decoded value of the request body based on the Content-Type pair.
-  /// The following decoders are supported by default (you may add extra decoders using [addDecoder], do not manipulate [decoders] directly.)
+  /// The following decoders are supported by default (you may add extra decoders using [addDecoder], do not manipulate [_decoders] directly.)
   ///
   ///       application/json
   ///       application/x-www-form-urlencoded
   ///       text/*
   ///
-  static Map<String, Map<String, Function>> decoders = {
+  static Map<String, Map<String, Function>> _decoders = {
     "application" : {
       "json" : _jsonDecoder,
       "x-www-form-urlencoded" : _wwwFormURLEncodedDecoder
@@ -30,18 +30,25 @@ class HTTPBodyDecoder {
 
   /// Adds a decoder for HTTP Request Bodies.
   ///
-  /// Associated a [decoder] function for the primary and subtype of [type]. This will replace an existing decoder if one exists.
-  /// A [decoder] must return the decoded data as a [Future]. It takes a single [HttpRequest] as an argument. For example, the JSON encoder
+  /// Adds a [decoder] function for [type] when reading [HttpRequest]s. Decoders are used by [Request]s to decode their body
+  /// into the format indicated by the request's Content-Type, e.g. application/json. Decoding are most often initiated by invoking [Request.decodeBody], but can also be used more directly
+  /// via [decode]. By default, there are decoders for the following content types:
+  ///
+  ///       application/json
+  ///       application/x-www-form-urlencoded
+  ///       text/*
+  ///
+  /// This method will replace an existing decoder if one exists. A [decoder] must return the decoded data as a [Future]. It takes a single [HttpRequest] as an argument. For example, the JSON encoder
   /// is implemented similar to the following:
   ///
   ///       Future<dynamic> jsonDecoder(HttpRequest req) async {
   ///         return JSON.decode(await UTF8.decodeStream(req));
   ///       }
   static void addDecoder(ContentType type, Future<dynamic> decoder(HttpRequest req)) {
-    var innerMap = decoders[type.primaryType];
+    var innerMap = _decoders[type.primaryType];
     if (innerMap == null) {
       innerMap = {};
-      decoders[type.primaryType] = innerMap;
+      _decoders[type.primaryType] = innerMap;
     }
 
     innerMap[type.subType] = decoder;
@@ -70,7 +77,7 @@ class HTTPBodyDecoder {
   /// Decodes an [HttpRequest]'s body based on its Content-Type.
   ///
   /// This method will return the decoded object as a [Future]. The [HttpRequest]'s Content-Type is evaluated
-  /// for a matching decoding function in [decoders]. If no such decoder is found, the body is returned as a [List] of bytes, where each byte is an [int].
+  /// for a matching decoding function in [_decoders]. If no such decoder is found, the body is returned as a [List] of bytes, where each byte is an [int].
   static Future<dynamic> decode(HttpRequest request) async {
     if (request.headers.contentType == null) {
       return _binaryDecoder(request);
@@ -80,7 +87,7 @@ class HTTPBodyDecoder {
     var subType = request.headers.contentType.subType;
 
 
-    var outerMap = decoders[primaryType];
+    var outerMap = _decoders[primaryType];
     if (outerMap == null) {
       return _binaryDecoder(request);
     }
