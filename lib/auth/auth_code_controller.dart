3e12560d495a019447f76963ce32741898e6bb12
Sean Swezey <sean.swezey@stablekernel.com>
2016-08-05 10:54:17 -0400

    Fix sloppy documentation changes

--- lib/auth/auth_code_controller.dart
+++ lib/auth/auth_code_controller.dart
@@ -4,7 +4,7 @@ class AuthCodeController extends HTTPController {
 
   /// Creates a new instance of an [AuthCodeController].
   ///
-  /// An [AuthCodeController] requires an [AuthenticationServer], with the specified [ResourceOwner] and [TokenType] instance
+  /// An [AuthCodeController] requires an [AuthenticationServer], with the specified [ResourceOwner] and [AuthCodeType] instance
   /// types. These types will be used when communicating with the [AuthenticationServer] for creating and refreshing
   /// authentication tokens.
   ///
