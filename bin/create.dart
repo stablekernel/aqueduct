26ab916b7279d0944c27fc6c7453b1307d144eaa
Joe Conway <joe.conway@stablekernel.com>
2016-08-14 16:29:45 -0400

    wip

--- bin/create.dart
+++ bin/create.dart
@@ -1,30 +1,40 @@
 import 'dart:io';
 import 'dart:async';
+import 'dart:mirrors';
+import 'dart:isolate';
+import 'package:args/args.dart';
 
 Future main(List<String> args) async {
-  var useBranch = "master";
-  if (args.length == 2) {
-    useBranch = args.last;
+  var packageURI = await Isolate.resolvePackageUri(new Uri(scheme: "package", path: "aqueduct/aqueduct.dart"));
+  var parser = new ArgParser(allowTrailingOptions: false);
+  parser.addOption("template", abbr: "t", defaultsTo: "default", help: "Name of the template. Defaults to default. Available options are: default");
+  parser.addOption("name", abbr: "n", help: "Name of project in snake_case.");
+  parser.addFlag("local", abbr: "l", defaultsTo: false, hide: true);
+
+  var argValues = parser.parse(args);
+
+  if (!isSnakeCase(argValues["name"])) {
+    print("Invalid project name\n${parser.usage}");
+    return;
   }
 
-  Directory destDirectory = destinationDirectoryFromPath(args.first);
+  Directory destDirectory = destinationDirectoryFromPath(argValues["name"]);
   if (destDirectory.existsSync()) {
-    throw new Exception("Error: file already exists at path ${destDirectory.path}.");
+    print("Error: file already exists at path ${destDirectory.path}.");
+    return;
   }
 
-  var sourceDirectory = new Directory("${destDirectory.path}_temp");
-  await cloneTemplateToDirectory(sourceDirectory, branch: useBranch);
+  var sourceDirectory = new Directory("${packageURI.path}/example/templates/${argValues["template"]}");
+  if (!sourceDirectory.existsSync()) {
+    print("Error: no template named ${argValues["template"]}");
+    return;
+  }
 
-  var projectName = projectNameFromPath(destDirectory.path);
-  await copyProjectFiles(destDirectory, sourceDirectory, projectName);
+  await copyProjectFiles(destDirectory, sourceDirectory, argValues["name"]);
 
   await createProjectSpecificFiles(destDirectory.path);
 
-  print("${projectName} exists at ${destDirectory.path}");
-}
-
-String readmeContents() {
-  return "###Setup\nEnsure that database access has been setup via the instructions here: https://github.com/stablekernel/wildfire/blob/master/README.md";
+  print("${argValues["name"]} created at ${destDirectory.path}");
 }
 
 bool shouldIncludeItem(FileSystemEntity entity) {
@@ -36,7 +46,6 @@ bool shouldIncludeItem(FileSystemEntity entity) {
     "tasks.xml",
     "vcs.xml",
     "ignite.dart",
-    "README.md",
     "ignite_test.dart"
   ];
 
@@ -68,80 +77,48 @@ void interpretContentFile(String projectName, Directory destinationDirectory, Fi
   }
 }
 
-void copyDirectory(String projectName, Directory destinationParentDirectory,
-    Directory sourceDirectory) {
-  var sourceDirectoryName = sourceDirectory.uri.pathSegments[sourceDirectory.uri
-      .pathSegments.length - 2];
-  var destDir = new Directory(
-      destinationParentDirectory.path + "/" + sourceDirectoryName);
+void copyDirectory(String projectName, Directory destinationParentDirectory, Directory sourceDirectory) {
+  var sourceDirectoryName = sourceDirectory.uri.pathSegments[sourceDirectory.uri.pathSegments.length - 2];
+  var destDir = new Directory(destinationParentDirectory.path + "/" + sourceDirectoryName);
+
   destDir.createSync();
+
   sourceDirectory.listSync().forEach((f) {
     interpretContentFile(projectName, destDir, f);
   });
 }
 
-void copyFile(String projectName, Directory destinationDirectory,
-    File sourceFile) {
-  var path = destinationDirectory.path + "/" +
-      fileNameForFile(projectName, sourceFile);
+void copyFile(String projectName, Directory destinationDirectory, File sourceFile) {
+  var path = destinationDirectory.path + "/" + fileNameForFile(projectName, sourceFile);
   var contents = sourceFile.readAsStringSync();
 
-  var lowercase = lowercaseProjectName(projectName);
-  var uppercase = uppercaseProjectName(projectName);
-  contents = contents.replaceAll("wildfire", "$lowercase");
-  contents = contents.replaceAll("Wildfire", "$uppercase");
-
+  contents = contents.replaceAll("wildfire", projectName);
+  contents = contents.replaceAll("Wildfire", camelCaseFromSnakeCase(projectName));
 
   var outputFile = new File(path);
   outputFile.createSync();
   outputFile.writeAsStringSync(contents);
 }
 
-String lowercaseProjectName(String projectName) {
-  return projectName.toLowerCase();
-}
-
-String uppercaseProjectName(String projectName) {
-  return projectName.replaceRange(
-      0, 1, projectName.substring(0, 1).toUpperCase());
-}
-
 String fileNameForFile(String projectName, File sourceFile) {
-  var lowercase = lowercaseProjectName(projectName);
   var fileName = sourceFile.uri.pathSegments.last;
-  fileName = fileName.replaceFirst("S___LOWER_APPLICATION_NAME___", lowercase);
 
-  if (fileName == "wildfire.iml") {
-    fileName = "$lowercase.iml";
-  } else if (fileName == "wildfire.dart") {
-    fileName = "$lowercase.dart";
-  }
+  fileName = fileName.replaceFirst("wildfire", projectName);
 
   return fileName;
-}
-
-void cloneTemplateToDirectory(Directory dir, {String branch: "master"}) {
-  print("Fetching template source from git...");
-  Process.runSync("git", [
-    "clone", "https://github.com/stablekernel/wildfire.git", dir.path
-  ]);
-
-  if (branch != "master") {
-    Process.runSync("git", ["checkout", branch], workingDirectory: dir.path);
-  }
 }
 
 Directory destinationDirectoryFromPath(String pathString) {
   if (pathString.startsWith("/")) {
     return new Directory(pathString);
-  } else {
-    var currentDirPath = Directory.current.uri.toFilePath();
-    if (!currentDirPath.endsWith("/")) {
-      currentDirPath += "/";
-    }
-    currentDirPath += pathString;
-    return new Directory(currentDirPath);
   }
+  var currentDirPath = Directory.current.uri.toFilePath();
+  if (!currentDirPath.endsWith("/")) {
+    currentDirPath += "/";
+  }
+  currentDirPath += pathString;
+
+  return new Directory(currentDirPath);
 }
 
 String projectNameFromPath(String pathString) {
@@ -156,11 +133,6 @@ String projectNameFromPath(String pathString) {
 }
 
 Future createProjectSpecificFiles(String directoryPath) async {
-  var readmePath = new File(directoryPath + "/README.md");
-  var readmeSink = readmePath.openWrite();
-  readmeSink.write(readmeContents());
-  await readmeSink.close();
-
   // Put config.yaml in, but then also put it in the gitignore.
   var configSrcPath = new File(directoryPath + "/config.yaml.src");
   configSrcPath.copySync(new File(directoryPath + "/config.yaml").path);
@@ -177,16 +149,30 @@ void copyProjectFiles(Directory destinationDirectory, Directory sourceDirectory,
   try {
     print("Supplying project values...");
     destinationDirectory.createSync();
+
     new Directory(sourceDirectory.path).listSync().forEach((f) {
       interpretContentFile(projectName, destinationDirectory, f);
     });
 
     print("Fetching dependencies...");
-    var result = Process.runSync("pub", ["get"], workingDirectory: destinationDirectory.path);
+    Process.runSync("pub", ["get"], workingDirectory: destinationDirectory.path);
   } catch (e) {
     Process.runSync("rm", ["-rf", destinationDirectory.path]);
     print("${e}");
-  } finally {
-    Process.runSync("rm", ["-rf", sourceDirectory.path]);
   }
+}
+
+bool isSnakeCase(String string) {
+  return string.toLowerCase() == string;
+}
+
+String camelCaseFromSnakeCase(String string) {
+  return string
+      .split("_")
+      .map((str) {
+        var firstChar = str.substring(0, 1);
+        var remainingString = str.substring(1, str.length);
+        return firstChar.toUpperCase() + remainingString;
+      })
+      .join("");
 }
