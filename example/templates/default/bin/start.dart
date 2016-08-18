65a96c713f61fae4b82b919a09ab5bf38bd8d9a0
Joe Conway <joe.conway@stablekernel.com>
2016-08-17 21:45:52 -0400

    Removed spurious comment

--- example/templates/default/bin/start.dart
+++ example/templates/default/bin/start.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:wildfire/wildfire.dart';
 import 'dart:io';
 
