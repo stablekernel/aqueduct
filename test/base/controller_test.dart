5439d5d721f1c70cc795d26ee2cf9c25db6aae29
Joe Conway <joeconway3@gmail.com>
2016-12-02 23:01:51 -0500

    dartfmt

--- test/base/controller_test.dart
+++ test/base/controller_test.dart
@@ -432,46 +432,49 @@ void main() {
       expect(JSON.decode(resp.body)["error"], contains("Shaqs"));
     });
 
-    test("May only be one query parameter if arg type is not List<T>", () async {
+    test("May only be one query parameter if arg type is not List<T>",
+        () async {
       server = await enableController("/a", DuplicateParamController);
       var resp = await http
           .get("http://localhost:4040/a?list=a&list=b&single=x&single=y");
 
       expect(resp.statusCode, 400);
 
-      expect(JSON.decode(resp.body)["error"], "Duplicate parameter for non-List parameter type");
+      expect(JSON.decode(resp.body)["error"],
+          "Duplicate parameter for non-List parameter type");
     });
 
-    test("Can be more than one query parameters for arg type that is List<T>", () async {
+    test("Can be more than one query parameters for arg type that is List<T>",
+        () async {
       server = await enableController("/a", DuplicateParamController);
-      var resp = await http
-          .get("http://localhost:4040/a?list=a&list=b&single=x");
+      var resp =
+          await http.get("http://localhost:4040/a?list=a&list=b&single=x");
 
       expect(resp.statusCode, 200);
 
       expect(JSON.decode(resp.body), {
-        "list" : ["a", "b"],
-        "single" : "x"
+        "list": ["a", "b"],
+        "single": "x"
       });
     });
 
-    test("Can be exactly one query parameter for arg type that is List<T>", () async {
+    test("Can be exactly one query parameter for arg type that is List<T>",
+        () async {
       server = await enableController("/a", DuplicateParamController);
-      var resp = await http
-          .get("http://localhost:4040/a?list=a&single=x");
+      var resp = await http.get("http://localhost:4040/a?list=a&single=x");
 
       expect(resp.statusCode, 200);
 
       expect(JSON.decode(resp.body), {
-        "list" : ["a"],
-        "single" : "x"
+        "list": ["a"],
+        "single": "x"
       });
     });
 
-    test("Missing required List<T> query parameter still returns 400", () async {
+    test("Missing required List<T> query parameter still returns 400",
+        () async {
       server = await enableController("/a", DuplicateParamController);
-      var resp = await http
-          .get("http://localhost:4040/a?single=x");
+      var resp = await http.get("http://localhost:4040/a?single=x");
 
       expect(resp.statusCode, 400);
 
@@ -669,13 +672,9 @@ class ContentTypeController extends HTTPController {
 
 class DuplicateParamController extends HTTPController {
   @httpGet
-  Future<Response> getThing(
-      @HTTPQuery("list") List<String> list,
+  Future<Response> getThing(@HTTPQuery("list") List<String> list,
       @HTTPQuery("single") String single) async {
-    return new Response.ok({
-      "list" : list,
-      "single" : single
-    });
+    return new Response.ok({"list": list, "single": single});
   }
 }
 
