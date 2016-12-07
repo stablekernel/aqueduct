e8512959b4411e57cadcde2a6f98580fae88eeba
Joe Conway <joeconway3@gmail.com>
2016-12-07 14:19:55 -0500

    Changing script error

--- ci/after_script.sh
+++ ci/after_script.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   curl -s https://codecov.io/bash > .codecov
   chmod +x .codecov
   ./.codecov -f lcov.info -X xcode
