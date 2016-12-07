9dde4e91876f18d65cea3add4aa997b5fe64ca41
Joe Conway <joeconway3@gmail.com>
2016-12-07 13:27:10 -0500

    Getting code coverage to only run on master (#151)
    * Getting code coverage to only run on master
    
    * Moving scripts into ci dir
    
    * Moving scripts into ci dir

---
+++ ci/script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pub run test -j 1 -r expanded
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
+  dart_codecov_generator --report-on=lib/ --verbose --no-html
+fi
