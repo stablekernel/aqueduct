dae00899b6a39cf4b283a3cc645b82fa581fc0e0
Joe Conway <joeconway3@gmail.com>
2016-12-07 14:42:47 -0500

    Debugging travis script

--- ci/script.sh
+++ ci/script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    echo 'ok'
+fi
+
 pub run test -j 1 -r expanded
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
