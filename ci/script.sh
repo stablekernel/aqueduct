d04a82da16021a39c5d7766bd56441775cb07910
Joe Conway <joeconway3@gmail.com>
2017-01-19 09:41:28 -0500

    Fixing ci script

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 pub run test -j 1 -r expanded
+
+if [ $? -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
+
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
   dart_codecov_generator --report-on=lib/ --verbose --no-html
