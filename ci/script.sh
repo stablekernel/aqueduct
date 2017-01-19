3ce37037c80b0fe19965a252d15a430409762267
Joe Conway <joeconway3@gmail.com>
2017-01-19 10:12:20 -0500

    Even better

--- ci/script.sh
+++ ci/script.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-pub run test -j 1 -r expanded
+set -e
 
-if [ $? -ne 0 ]
-then
-    exit $?
-fi
+pub run test -j 1 -r expanded
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
