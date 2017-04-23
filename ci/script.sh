40822f7ae2d7b12ffe11eaf02707df1c9aaaf99e
Joe Conway <joeconway3@gmail.com>
2017-04-22 20:54:40 -0400

    Disabling code coverage until dart issue is resolved

--- ci/script.sh
+++ ci/script.sh
@@ -4,7 +4,7 @@ set -e
 
 pub run test -j 1 -r expanded
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
-  dart_codecov_generator --report-on=lib/ --verbose --no-html
-fi
+#if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+#  pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
+#  dart_codecov_generator --report-on=lib/ --verbose --no-html
+#fi
