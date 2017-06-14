8a35b047939a0a4285780dbb5f7bd1da83278056
Joe Conway <joeconway3@gmail.com>
2017-06-13 21:55:29 -0400

    Add tests for router, allow HTTPFileController to provide 404 implementation other than default 404

--- test/base/file_controller_test.dart
+++ test/base/file_controller_test.dart
@@ -195,7 +195,6 @@ void main() {
     expect(JSON.decode(response.body), {"k":"v"});
   });
 
-
   group("Default caching", () {
     test("Uncached file has no cache-control", () async {
       var response = await getCacheableFile("file.json");
