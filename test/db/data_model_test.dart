048064fc19a5d41168931eae0d99d0784593a81a
Joe Conway <joeconway3@gmail.com>
2017-08-02 22:07:52 -0400

    Adds entity unique to data model

--- test/db/data_model_test.dart
+++ test/db/data_model_test.dart
@@ -538,6 +538,14 @@ void main() {
       expect(e.uniquePropertySet.contains(e.properties["b"]), true);
     });
 
+    test("Add ManagedTableAttributes to persistent type with unique list makes instances unique for those columns, where column is foreign key relationship", () {
+      var dm = new ManagedDataModel([MultiUniqueBelongsTo, MultiUniqueHasA]);
+      var e = dm.entityForType(MultiUniqueBelongsTo);
+      expect(e.unique.length, 2);
+      expect(e.unique.contains(e.properties["rel"]), true);
+      expect(e.unique.contains(e.properties["b"]), true);
+    });
+
     test("Add ManagedTableAttributes to persistent type with only single element in unique list throws exception, warns to use ManagedColumnAttributes", () {
       try {
         new ManagedDataModel([MultiUniqueFailureSingleElement]);
