23d5b5cbfdfc7727e4de75371f386ed0ac67a145
Joe Conway <joeconway3@gmail.com>
2017-12-01 14:25:54 -0500

    Clear directory before sync

--- ci/after_script.sh
+++ ci/after_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [[ "$STAGE" == "coverage" && "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
-  $HOME/.local/bin/aws s3 sync coverage_json s3://aqueduct-coverage-storage/coverage --delete
-
   if [ -a coverage/lcov.info ]; then
     curl -s https://codecov.io/bash > .codecov
     chmod +x .codecov
     ./.codecov -f coverage/lcov.info -X xcode
+
+    rm -rf coverage_json
+    mkdir coverage_json
+    $HOME/.local/bin/aws s3 sync coverage_json s3://aqueduct-coverage-storage/coverage --delete
   fi
 fi
 
