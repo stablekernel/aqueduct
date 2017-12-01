95eb0ed9b72f995fcfab0dd8c7ce8a80d1883c91
Joe Conway <joeconway3@gmail.com>
2017-12-01 14:20:08 -0500

    Add --delete flag to s3 sync for coverage script

--- ci/after_script.sh
+++ ci/after_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$STAGE" == "coverage" && "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
-  $HOME/.local/bin/aws s3 sync coverage_json s3://aqueduct-coverage-storage/coverage
+  $HOME/.local/bin/aws s3 sync coverage_json s3://aqueduct-coverage-storage/coverage --delete
 
   if [ -a coverage/lcov.info ]; then
     curl -s https://codecov.io/bash > .codecov
