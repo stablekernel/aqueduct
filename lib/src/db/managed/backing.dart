ffee58c16cac00f15de19226eb4c50e465491d0d
Joe Conway <joeconway3@gmail.com>
2018-03-11 20:54:13 -0400

    remove unused managedmatcherbacking

--- lib/src/db/managed/backing.dart
+++ lib/src/db/managed/backing.dart
@@ -27,56 +27,6 @@ class ManagedValueBacking extends ManagedBacking {
   }
 }
 
-class ManagedMatcherBacking extends ManagedBacking {
-  @override
-  Map<String, dynamic> valueMap = {};
-
-  @override
-  dynamic valueForProperty(ManagedPropertyDescription property) {
-    if (!valueMap.containsKey(property.name)) {
-      // For any relationships, automatically insert them into valueMap
-      // so that their properties can be accessed when building queries.
-      if (property is ManagedRelationshipDescription) {
-        if (property.relationshipType == ManagedRelationshipType.hasMany) {
-          valueMap[property.name] = new ManagedSet()
-            ..entity = property.destinationEntity;
-        } else if (property.relationshipType == ManagedRelationshipType.hasOne ||
-            property.relationshipType == ManagedRelationshipType.belongsTo) {
-          valueMap[property.name] = property.destinationEntity.newInstance(backing: new ManagedMatcherBacking());
-        }
-      }
-    }
-
-    return valueMap[property.name];
-  }
-
-  @override
-  void setValueForProperty(ManagedPropertyDescription property, dynamic value) {
-    if (value == null) {
-      valueMap.remove(property.name);
-      return;
-    }
-
-    if (value is PredicateExpression) {
-      if (property is ManagedRelationshipDescription) {
-        var innerObject = valueForProperty(property);
-        if (innerObject is ManagedObject) {
-          innerObject[innerObject.entity.primaryKey] = value;
-        } else if (innerObject is ManagedSet) {
-          innerObject.haveAtLeastOneWhere[innerObject.entity.primaryKey] =
-              value;
-        }
-      } else {
-        valueMap[property.name] = value;
-      }
-    } else {
-      final typeName = MirrorSystem.getName(property.entity.instanceType.simpleName);
-
-      throw new ArgumentError("Invalid query matcher assignment. Tried assigning value to 'Query<$typeName>.where.${property.name}'. Wrap value in 'whereEqualTo()'.");
-    }
-  }
-}
-
 class ManagedAccessTrackingBacking extends ManagedBacking {
   List<KeyPath> keyPaths;
   KeyPath workingKeyPath;
