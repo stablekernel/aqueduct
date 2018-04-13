f046c0fbcc772cf29cd6d1f901e842996c661e3f
Joe Conway <joeconway3@gmail.com>
2018-04-12 22:26:44 -0400

    Fix issue where any error when building migration source was reported as a fialed compile

--- lib/src/db/schema/migration_source.dart
+++ lib/src/db/schema/migration_source.dart
@@ -16,41 +16,34 @@ class MigrationSource {
   }
 
   factory MigrationSource.fromFile(Uri uri) {
+    CompilationUnit fileUnit;
     try {
-      final fileUnit = parseDartFile(uri.path);
-
-      final sources = fileUnit.declarations
-        .where((u)
-      => u is ClassDeclaration)
-        .map((cu)
-      => cu as ClassDeclaration)
-        .where((ClassDeclaration classDecl)
-      {
-        return classDecl.extendsClause.superclass.name.name == "Migration";
-      }).map((cu)
-      {
-        final code = cu.toSource();
-        final offset = cu.name.offset - cu.offset;
-        return new MigrationSource(code, uri, offset, offset + cu.name.length);
-      }).toList();
-
-      if (sources.length != 1) {
-        throw new StateError("Invalid migration file. Must contain exactly one 'Migration' subclass. File: '$uri'.");
-      }
-      return sources.first;
+      fileUnit = parseDartFile(uri.path);
     } catch (_) {
-      throw new StateError("Migration file '${uri.path}' failed to compile. If this file was generated and required additional input, "
-        "search for the phrase '<<set>>' in the file and replace it with an appropriate expression.");
+      throw new StateError(
+          "Migration file '${uri.path}' failed to compile. If this file was generated and required additional input, "
+          "search for the phrase '<<set>>' in the file and replace it with an appropriate expression.");
     }
+
+    final sources = fileUnit.declarations
+        .where((u) => u is ClassDeclaration)
+        .map((cu) => cu as ClassDeclaration)
+        .where((ClassDeclaration classDecl) {
+      return classDecl.extendsClause.superclass.name.name == "Migration";
+    }).map((cu) {
+      final code = cu.toSource();
+      final offset = cu.name.offset - cu.offset;
+      return new MigrationSource(code, uri, offset, offset + cu.name.length);
+    }).toList();
+
+    if (sources.length != 1) {
+      throw new StateError("Invalid migration file. Must contain exactly one 'Migration' subclass. File: '$uri'.");
+    }
+    return sources.first;
   }
 
   Map<String, dynamic> asMap() {
-    return {
-      "originalName": originalName,
-      "name": name,
-      "source": source,
-      "uri": uri
-    };
+    return {"originalName": originalName, "name": name, "source": source, "uri": uri};
   }
 
   static String combine(List<MigrationSource> sources) {
