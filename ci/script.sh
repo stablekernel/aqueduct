993064d84d2ceb0300a623d4085e0dbd6fe4ea1b
Joe Conway <joeconway3@gmail.com>
2018-04-27 23:14:37 -0400

    Move before script to inside script

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,15 @@
 
 set -e
 
+psql -c 'create user dart with createdb;' -U postgres
+psql -c "alter user dart with password 'dart';" -U postgres
+psql -c 'create database dart_test;' -U postgres
+psql -c 'grant all on database dart_test to dart;' -U postgres
+
+
 cd "$TEST_DIR"
+
+pub get
 
 if [[ "$STAGE" == "tests" ]]; then
   pub run test -j 1 -r expanded
