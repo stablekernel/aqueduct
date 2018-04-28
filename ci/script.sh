bb236457e1581afb01af146ba2b30c18e414e018
Joe Conway <joeconway3@gmail.com>
2018-04-27 23:49:29 -0400

    Remove conditional from script

--- ci/script.sh
+++ ci/script.sh
@@ -11,13 +11,11 @@ cd "$TEST_DIR"
 
 pub get
 
-if [[ "$STAGE" == "tests" ]]; then
-  pub run test -j 1 -r expanded
-fi
+pub run test -j 1 -r expanded
 
-if [[ "$STAGE" == "coverage" && "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
-  pub global activate -sgit https://github.com/stablekernel/aqueduct-coverage-tool.git
-  pub global run aqueduct_coverage_tool:main
-fi
+#if [[ "$TRAVIS_BUILD_STAGE_NAME" == "coverage" && "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
+#  pub global activate -sgit https://github.com/stablekernel/aqueduct-coverage-tool.git
+#  pub global run aqueduct_coverage_tool:main
+#fi
 
 cd ..
