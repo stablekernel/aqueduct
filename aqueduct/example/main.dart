fa1d616efcd6968424e99074b4c34514b526221d
Joe Conway <joeconway3@gmail.com>
2018-07-22 14:21:07 -0400

    update example

--- aqueduct/example/main.dart
+++ aqueduct/example/main.dart
@@ -1,16 +1,18 @@
 /*
+  This example demonstrates an HTTP application that uses the ORM and ORM-backed OAuth2 provider.
+
   For building and running non-example applications, install 'aqueduct' command-line tool.
 
       pub global activate aqueduct
       aqueduct create my_app
-      cd my_app/
-      aqueduct serve
 
   More examples available: https://github.com/stablekernel/aqueduct_examples
  */
 
 import 'dart:async';
+import 'dart:io';
 import 'package:aqueduct/aqueduct.dart';
+import 'package:aqueduct/managed_auth.dart';
 
 Future main() async {
   final app = Application<App>()
@@ -21,22 +23,96 @@ Future main() async {
 }
 
 class App extends ApplicationChannel {
+  ManagedContext context;
+  AuthServer authServer;
+
+  @override
+  Future prepare() async {
+    final config =
+        AppConfiguration.fromFile(File(options.configurationFilePath));
+    final db = config.database;
+    final persistentStore = PostgreSQLPersistentStore.fromConnectionInfo(
+        db.username, db.password, db.host, db.port, db.databaseName);
+    context = ManagedContext(
+        ManagedDataModel.fromCurrentMirrorSystem(), persistentStore);
+
+    authServer = AuthServer(ManagedAuthDelegate(context));
+  }
+
   @override
   Controller get entryPoint {
-    final router = Router();
-    router.route('/example/[:id]').link(() => ExampleController());
-    return router;
+    return Router()
+      ..route('/auth/token').link(() => AuthController(authServer))
+      ..route('/users/[:id]')
+          .link(() => Authorizer(authServer))
+          .link(() => UserController(context, authServer));
   }
 }
 
-class ExampleController extends ResourceController {
+class UserController extends ResourceController {
+  UserController(this.context, this.authServer);
+
+  final ManagedContext context;
+  final AuthServer authServer;
+
   @Operation.get()
-  Future<Response> getExamples() async {
-    return Response.ok([]);
+  Future<Response> getUsers() async {
+    final query = Query<User>(context);
+    return Response.ok(await query.fetch());
   }
 
   @Operation.get('id')
-  Future<Response> getExampleById(@Bind.path('id') int id) async {
-    return Response.ok({'id': id});
+  Future<Response> getUserById(@Bind.path('id') int id) async {
+    final q = Query<User>(context)..where((o) => o.id).equalTo(id);
+    final user = await q.fetchOne();
+
+    if (user == null) {
+      return Response.notFound();
+    }
+
+    return Response.ok(user);
   }
+
+  @Operation.post()
+  Future<Response> createUser(@Bind.body() User user) async {
+    if (user.username == null || user.password == null) {
+      return Response.badRequest(
+          body: {"error": "username and password required."});
+    }
+
+    final salt = AuthUtility.generateRandomSalt();
+    final hashedPassword = authServer.hashPassword(user.password, salt);
+
+    final query = Query<User>(context)
+      ..values = user
+      ..values.hashedPassword = hashedPassword
+      ..values.salt = salt
+      ..values.email = user.username;
+
+    final u = await query.insert();
+    final token = await authServer.authenticate(
+        u.username,
+        query.values.password,
+        request.authorization.credentials.username,
+        request.authorization.credentials.password);
+
+    return AuthController.tokenResponse(token);
+  }
+}
+
+class AppConfiguration extends Configuration {
+  AppConfiguration.fromFile(File file) : super.fromFile(file);
+
+  DatabaseConfiguration database;
+}
+
+class User extends ManagedObject<_User>
+    implements _User, ManagedAuthResourceOwner<_User> {
+  @Serialize(input: true, output: false)
+  String password;
+}
+
+class _User extends ResourceOwnerTableDefinition {
+  @Column(unique: true)
+  String email;
 }
