a58c3e80e156ef39567b8d843f84011263272220
Joe Conway <joeconway3@gmail.com>
2018-08-15 20:49:20 -0400

    set min of 1 isolate regardless of cpu count (#534)

--- aqueduct/templates/db_and_auth/bin/main.dart
+++ aqueduct/templates/db_and_auth/bin/main.dart
@@ -5,7 +5,8 @@ Future main() async {
       ..options.configurationFilePath = "config.yaml"
       ..options.port = 8888;
 
-  await app.start(numberOfInstances: Platform.numberOfProcessors ~/ 2);
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
 
   print("Application started on port: ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
