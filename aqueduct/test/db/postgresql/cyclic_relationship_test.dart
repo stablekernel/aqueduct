e04b8f21db88265c09675b72eae4e97fa885daad
Joe Conway <joeconway3@gmail.com>
2018-11-14 19:30:09 -0500

    tests for self-referencing tables

---
+++ aqueduct/test/db/postgresql/cyclic_relationship_test.dart
@@ -0,0 +1,350 @@
+import 'package:test/test.dart';
+import 'package:aqueduct/aqueduct.dart';
+import 'package:postgres/postgres.dart';
+import '../../helpers.dart';
+
+void main() {
+  justLogEverything();
+
+  ManagedContext context;
+
+  tearDown(() async {
+    await context?.close();
+    context = null;
+  });
+
+  group("Self-reference", () {
+    setUp(() async {
+      context = await contextWithModels([SelfRef]);
+    });
+
+    test("Insert an object that references an existing object", () async {
+      final parent =
+          await Query.insertObject(context, SelfRef()..name = "Parent");
+
+      var q = Query<SelfRef>(context)
+        ..values.name = "Child"
+        ..values.parent = parent;
+      final child = await q.insert();
+
+      expect(parent.name, "Parent");
+      expect(child.name, "Child");
+      expect(child.parent.id, parent.id);
+
+      q = Query<SelfRef>(context);
+      final all = await q.fetch();
+      expect(all.length, 2);
+    });
+
+    test("Update an object to reference itself", () async {
+      final parent =
+          await Query.insertObject(context, SelfRef()..name = "self");
+
+      var q = Query<SelfRef>(context)
+        ..where((s) => s.id).equalTo(parent.id)
+        ..values.parent = parent;
+      await q.updateOne();
+
+      q = Query<SelfRef>(context);
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          "id": parent.id,
+          "name": "self",
+          "parent": {"id": parent.id}
+        }
+      ]);
+    });
+
+    test("Join from table without foreign key", () async {
+      var q = Query<SelfRef>(context)..values.name = "Parent";
+      final parent = await q.insert();
+
+      await Query.insertObjects(
+          context,
+          ["a", "b", "c"].map((n) {
+            return SelfRef()
+              ..name = n
+              ..parent = parent;
+          }).toList());
+
+      q = Query<SelfRef>(context)
+        ..where((s) => s.id).equalTo(parent.id)
+        ..join(set: (s) => s.children)
+            .sortBy((s) => s.name, QuerySortOrder.ascending);
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          "id": parent.id,
+          "name": "Parent",
+          "parent": null,
+          "children": [
+            {
+              "id": isNotNull,
+              "name": "a",
+              "parent": {"id": parent.id}
+            },
+            {
+              "id": isNotNull,
+              "name": "b",
+              "parent": {"id": parent.id}
+            },
+            {
+              "id": isNotNull,
+              "name": "c",
+              "parent": {"id": parent.id}
+            },
+          ]
+        }
+      ]);
+    });
+
+    test("Join from table with foreign key", () async {
+      var q = Query<SelfRef>(context)..values.name = "Parent";
+      final parent = await q.insert();
+
+      final objs = await Query.insertObjects(
+          context,
+          ["a", "b", "c"].map((n) {
+            return SelfRef()
+              ..name = n
+              ..parent = parent;
+          }).toList());
+
+      q = Query<SelfRef>(context)
+        ..where((s) => s.id).equalTo(objs.first.id)
+        ..join(object: (s) => s.parent);
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          "id": objs.first.id,
+          "name": "a",
+          "parent": {"id": parent.id, "name": "Parent", "parent": null},
+        }
+      ]);
+    });
+
+    test("Join multiple times", () async {
+      var q = Query<SelfRef>(context)..values.name = "Parent";
+      final parent = await q.insert();
+
+      final objs = await Query.insertObjects(
+          context,
+          ["a", "b", "c"].map((n) {
+            return SelfRef()
+              ..name = n
+              ..parent = parent;
+          }).toList());
+
+      await Query.insertObject(
+          context,
+          SelfRef()
+            ..name = "x"
+            ..parent = objs.first);
+
+      q = Query<SelfRef>(context)
+        ..sortBy((s) => s.name, QuerySortOrder.ascending);
+      final inner = q.join(set: (s) => s.children)
+        ..sortBy((s) => s.name, QuerySortOrder.ascending);
+      inner.join(set: (s) => s.children);
+
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          "id": parent.id,
+          "name": "Parent",
+          "parent": null,
+          "children": [
+            {
+              "id": isNotNull,
+              "name": "a",
+              "parent": {"id": parent.id},
+              "children": [
+                {"id": isNotNull, "name": "x", "parent": isNotNull}
+              ]
+            },
+            {
+              "id": isNotNull,
+              "name": "b",
+              "parent": {"id": parent.id},
+              "children": []
+            },
+            {
+              "id": isNotNull,
+              "name": "c",
+              "parent": {"id": parent.id},
+              "children": []
+            },
+          ]
+        },
+        {
+          "id": isNotNull,
+          "name": "a",
+          "parent": {"id": parent.id},
+          "children": [
+            {
+              "id": isNotNull,
+              "name": "x",
+              "parent": {"id": isNotNull},
+              "children": []
+            }
+          ]
+        },
+        {
+          "id": isNotNull,
+          "name": "b",
+          "parent": {"id": parent.id},
+          "children": []
+        },
+        {
+          "id": isNotNull,
+          "name": "c",
+          "parent": {"id": parent.id},
+          "children": []
+        },
+        {
+          "id": isNotNull,
+          "name": "x",
+          "parent": {"id": isNotNull},
+          "children": []
+        }
+      ]);
+    });
+
+    test("Join with a where clause on the primary table", () async {
+      var q = Query<SelfRef>(context)..values.name = "Parent";
+      final parent = await q.insert();
+
+      final objs = await Query.insertObjects(
+          context,
+          ["a", "b", "c"].map((n) {
+            return SelfRef()
+              ..name = n
+              ..parent = parent;
+          }).toList());
+
+      await Query.insertObject(
+          context,
+          SelfRef()
+            ..name = "x"
+            ..parent = objs.first);
+
+      q = Query<SelfRef>(context)..where((s) => s.id).equalTo(parent.id);
+      q.join(set: (s) => s.children).sortBy((s) => s.name, QuerySortOrder.ascending);
+
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          "id": parent.id,
+          "name": "Parent",
+          "parent": null,
+          "children": [
+            {
+              "id": isNotNull,
+              "name": "a",
+              "parent": {"id": parent.id},
+            },
+            {
+              "id": isNotNull,
+              "name": "b",
+              "parent": {"id": parent.id},
+            },
+            {
+              "id": isNotNull,
+              "name": "c",
+              "parent": {"id": parent.id},
+            },
+          ]
+        }
+      ]);
+    });
+
+    test("Join with a where clause on the joined table", () async {
+      var q = Query<SelfRef>(context)..values.name = "Parent";
+      final parent = await q.insert();
+
+      final objs = await Query.insertObjects(
+        context,
+        ["a", "b", "c"].map((n) {
+          return SelfRef()
+            ..name = n
+            ..parent = parent;
+        }).toList());
+
+      await Query.insertObject(
+        context,
+        SelfRef()
+          ..name = "x"
+          ..parent = objs.first);
+
+      q = Query<SelfRef>(context);
+      q.join(set: (s) => s.children).where((s) => s.name).greaterThan("b");
+
+      final all = await q.fetch();
+      expect(all.map((s) => s.asMap()).toList(), [
+        {
+          'id': 1,
+          'name': 'Parent',
+          'parent': null,
+          'children': [{'id': 4, 'name': 'c', 'parent': {'id': 1}}]
+        },
+        {
+          'id': 2,
+          'name': 'a',
+          'parent': {'id': 1},
+          'children': [{'id': 5, 'name': 'x', 'parent': {'id': 2}}]
+        },
+        {'id': 5, 'name': 'x', 'parent': {'id': 2}, 'children': []},
+        {'id': 4, 'name': 'c', 'parent': {'id': 1}, 'children': []},
+        {'id': 3, 'name': 'b', 'parent': {'id': 1}, 'children': []}
+      ]);
+    });
+
+    test("Join with where clause on both the primary and joined table",
+        () async {
+          var q = Query<SelfRef>(context)..values.name = "Parent";
+          final parent = await q.insert();
+
+          final objs = await Query.insertObjects(
+            context,
+            ["a", "b", "c"].map((n) {
+              return SelfRef()
+                ..name = n
+                ..parent = parent;
+            }).toList());
+
+          await Query.insertObject(
+            context,
+            SelfRef()
+              ..name = "x"
+              ..parent = objs.first);
+
+          q = Query<SelfRef>(context)..where((s) => s.name).lessThan("a");
+          q.join(set: (s) => s.children).where((s) => s.name).greaterThan("b");
+
+          final all = await q.fetch();
+          expect(all.map((s) => s.asMap()).toList(), [
+            {
+              'id': 1,
+              'name': 'Parent',
+              'parent': null,
+              'children': [{'id': 4, 'name': 'c', 'parent': {'id': 1}}]
+            },
+          ]);
+        });
+  });
+}
+
+class SelfRef extends ManagedObject<_SelfRef> implements _SelfRef {}
+
+class _SelfRef {
+  @primaryKey
+  int id;
+
+  String name;
+
+  ManagedSet<SelfRef> children;
+
+  @Relate(#children)
+  SelfRef parent;
+}
