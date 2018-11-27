f2be9b0537b2738cc49b6fdb50c842e8717497d1
Jonathon Day <jonathon.day@stablekernel.com>
2018-11-27 10:43:43 -0500

    Duplicate QueryPredicate.and tests for QueryPredicate.or

--- aqueduct/test/db/predicate_test.dart
+++ aqueduct/test/db/predicate_test.dart
@@ -74,5 +74,60 @@ void main() {
       final combined = QueryPredicate.or([p1, p2, p3]);
       expect(combined.format, "(p=q OR a=b OR x=y)");
     });
+
+    test("Duplicate keys are replaced", () {
+      var p1 = QueryPredicate("p=@p", {"p": 1});
+      var p2 = QueryPredicate("p=@p", {"p": 2});
+      final combined = QueryPredicate.or([p1, p2]);
+      expect(combined.format, "(p=@p OR p=@p0)");
+      expect(combined.parameters, {"p": 1, "p0": 2});
+    });
+
+    test("Multiple duplicate keys are replaced", () {
+      var p1 = QueryPredicate("p=@p", {"p": 1});
+      var p2 = QueryPredicate("p=@p", {"p": 2});
+      var p3 = QueryPredicate("p=@p", {"p": 3});
+      final combined = QueryPredicate.or([p1, p2, p3]);
+      expect(combined.format, "(p=@p OR p=@p0 OR p=@p1)");
+      expect(combined.parameters, {"p": 1, "p0": 2, "p1": 3});
+    });
+
+    test("If empty list, return empty predicate", () {
+      expect(QueryPredicate.or([]).format, "");
+      expect(QueryPredicate.or([]).parameters, {});
+    });
+
+    test("If null, return empty predicate", () {
+      expect(QueryPredicate.or(null).format, "");
+      expect(QueryPredicate.or(null).parameters, {});
+    });
+
+    test("If only one element in list, return that element", () {
+      final valid = QueryPredicate("x=@a", {"a": 0});
+      final p = QueryPredicate.or([valid]);
+      expect(p.format, valid.format);
+      expect(p.parameters, valid.parameters);
+    });
+
+    test("If or'ing empty predicate, ignore it", () {
+      final valid = QueryPredicate("x=@a", {"a": 0});
+      final p = QueryPredicate.or([valid, QueryPredicate.empty()]);
+      expect(p.format, valid.format);
+      expect(p.parameters, valid.parameters);
+    });
+
+    test("If or'ing null predicate, ignore it", () {
+      final valid = QueryPredicate("x=@a", {"a": 0});
+      final p = QueryPredicate.or([valid, null]);
+      expect(p.format, valid.format);
+      expect(p.parameters, valid.parameters);
+    });
+
+    test("Or'ing predicate with no parameters", () {
+      final valid = QueryPredicate("x=y", null);
+      final p = QueryPredicate.or([valid]);
+      expect(p.format, valid.format);
+      expect(p.parameters, {});
+    });
   });
 }
