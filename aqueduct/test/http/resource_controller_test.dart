ba313a7fe2e9cdb1272f8bcc4ab0a1ff562ca832
Joe Conway <joeconway3@gmail.com>
2020-02-22 10:28:11 -0500

    remove import

--- aqueduct/test/http/resource_controller_test.dart
+++ aqueduct/test/http/resource_controller_test.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import "dart:core";
 import "dart:io";
-import 'dart:mirrors';
 
 import 'package:aqueduct/aqueduct.dart';
 import 'package:http/http.dart' as http;
