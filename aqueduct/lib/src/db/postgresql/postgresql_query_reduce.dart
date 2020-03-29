40148931677a4044da7e3bcfa24cc57b6fefb168
Slavi Boyanov <slavi.boyanov@gmail.com>
2020-03-29 07:13:04 +0300

    Joins are taken in account for reduce queries

--- aqueduct/lib/src/db/postgresql/postgresql_query_reduce.dart
+++ aqueduct/lib/src/db/postgresql/postgresql_query_reduce.dart
@@ -2,15 +2,17 @@ import 'dart:async';
 
 import '../managed/object.dart';
 import '../query/query.dart';
+import 'builders/column.dart';
 import 'postgresql_persistent_store.dart';
 import 'postgresql_query.dart';
 import 'query_builder.dart';
 
 class PostgresQueryReduce<T extends ManagedObject>
     extends QueryReduceOperation<T> {
-  PostgresQueryReduce(this.query);
+  PostgresQueryReduce(this.query) : builder = PostgresQueryBuilder(query);
 
   final PostgresQuery<T> query;
+  final PostgresQueryBuilder builder;
 
   @override
   Future<double> average(num selector(T object)) {
@@ -38,14 +40,19 @@ class PostgresQueryReduce<T extends ManagedObject>
   }
 
   String _columnName(dynamic selector(T object)) {
-    return query.entity.identifyAttribute(selector).name;
+    final property = query.entity.identifyAttribute(selector);
+    final columnBuilder = ColumnBuilder(builder, property);
+    return columnBuilder.sqlColumnName(withTableNamespace: true);
   }
 
   Future<U> _execute<U>(String function) async {
-    final builder = PostgresQueryBuilder(query);
     final buffer = StringBuffer();
     buffer.write("SELECT $function ");
     buffer.write("FROM ${builder.sqlTableName} ");
+
+    if (builder.containsJoins) {
+      buffer.write("${builder.sqlJoin} ");
+    }
 
     if (builder.sqlWhereClause != null) {
       buffer.write("WHERE ${builder.sqlWhereClause} ");
